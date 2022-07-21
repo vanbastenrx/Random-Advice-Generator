7ddd244ccf280dea5778b378718491dc098dcdab
vanbastenrx <vanbastendev@gmail.com>
2022-07-21 01:39:56 -0300

    media querie

--- script.js
+++ script.js
@@ -7,25 +7,26 @@ let tipId = document.querySelector('.advice-id');
 let tipMessage = document.querySelector('.advice-message');
 const img = document.querySelector('.img');
 
-const getAdivisorTip = async () => {
+const getAdvice = async () => {
   try {
   const response = await fetch(`${API_URL}`);
   const data = await response.json();
-  renderTip(data);  
+  renderAdvice(data);  
   } catch (err) {
     console.error(err)
   }
 };
 
-const renderTip = (data) => {
+const renderAdvice = (data) => {
   tipId.innerHTML = `ADVICE #${Object.entries(data.slip)[0][1]}`;
   tipMessage.innerHTML = `"${Object.entries(data.slip)[1][1]}"`;
   img.style.opacity = .2;
 };
 
+window.onload = getAdvice();
 
 btn.addEventListener('click', (e) => {
   e.preventDefault();
-  getAdivisorTip();
+  getAdvice();
 });
  
